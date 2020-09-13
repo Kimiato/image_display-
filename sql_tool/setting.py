1c4f84391acbef99ac07d12ac29d5f8dca51461c
yizhi <yizhi@binmade.cn>
2020-09-13 21:50:17 +0800

    feat: init

---
+++ sql_tool/setting.py
@@ -0,0 +1,6 @@
+database_setting = {
+    "database_host": '',  # 数据库ip
+    "database_port": '',  # 数据库端口
+    "database_user": '',  # 用户名
+    'database_password': '',  # 密码
+}
--- sql_tool/sql_tools.py
+++ sql_tool/sql_tools.py
@@ -1,6 +1,23 @@
 import os
+from sql_tool.setting import database_setting
 import pymysql
 
+class DataLoader:
+    def __init__(self):
+        pass
+
+    def connect_database(self):
+        pass
+
+    def create_table(self):
+        pass
+
+    def write_data(self):
+        pass
+
+    def get_file_pwd(self):
+        pass
+    
 #连接数据库的ip，账号，密码，库名
 db = pymysql.connect("localhost", "root", "b081db1aeae010f1", "images")
 #创建游标
